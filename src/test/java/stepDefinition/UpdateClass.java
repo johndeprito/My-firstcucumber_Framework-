5d78753a4762de31a7c87cb4843f7f377888444f
Prito <reojohn21@gmail.com>
2022-05-10 19:37:02 +0530

    This has been success

--- src/test/java/stepDefinition/UpdateClass.java
+++ src/test/java/stepDefinition/UpdateClass.java
@@ -10,29 +10,29 @@ import io.cucumber.java.en.Then;
 
 public class UpdateClass extends Wrapperclass {
 	//Click Edit
-	@Given("Click the edit")
+	@And("Click the Edit button")
 	public void edit() {
 		WebElement edit = locateElement("linkText", "Edit");
 		edit.click();
 	}
 	//update the existing given data
-	@And("Clear the existingName and replace another name")
+	@And("Clear the giveName & Replace the another Name you want")
 	public void update() {
 		WebElement replace = locateElement("xpath", "//input[@name='givenName']");
 		replace.clear();
 		replace.sendKeys("JDP");
 	}
 	//Clear the middleName
-	@And("Clear the middle Name")
+	@And("Clear the middleName")
 	public void mid() {
 		WebElement middle = locateElement("xpath", "//input[@name='middleName']");
 		middle.clear();
 		}
 	//Save the update record
-	@Then("Save the record")
+	@Then("Save Form")
 	public void save() {
-		WebElement saveR = locateElement("xpath", "//input[@name='middleName']");
-		saveR.clear();
+		WebElement saveR = locateElement("linkText", "Save Form");
+		saveR.click();
 	}
 	//submit the record
 	@And("Submit the update record")
